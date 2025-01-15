19af24daea4d2e345f23c090f7d547aa95a919ed
Gustavo Leck <gustavoleck@hotmail.com>
2025-01-15 20:28:19 -0300

    firstDeploy

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/GustavoLeck/go_teste_2.git
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(teste())
+}
+
+func teste() string {
+	return "teste"
+}
